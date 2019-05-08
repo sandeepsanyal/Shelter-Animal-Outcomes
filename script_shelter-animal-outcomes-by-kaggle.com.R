62ede88be98500261a34ff98fec446ec7dac5438
sandeepsanyal <46559334+sandeepsanyal@users.noreply.github.com>
2019-05-08 21:36:37 +0530

    test - arranging code

--- script_shelter-animal-outcomes-by-kaggle.com.R
+++ script_shelter-animal-outcomes-by-kaggle.com.R
@@ -2,14 +2,18 @@
 ############################# importing training & testing data ##############################
 ##############################################################################################
 # read train_data
-train = read.csv("D:/Study/Case Studies/kaggle.com/Shelter-Animal-Outcomes-by-kaggle.com/original_data/train.csv", stringsAsFactors=FALSE)
+train = read.csv(file = "D:/Study/Case Studies/kaggle.com/Shelter-Animal-Outcomes-by-kaggle.com/original_data/train.csv",
+                 stringsAsFactors=FALSE)
 Dataset = c(rep("train", length(train[,1])))
-train=data.frame(train,Dataset, stringsAsFactors = FALSE)
+train=data.frame(train,Dataset,
+                 stringsAsFactors = FALSE)
 # read test_data
-test = read.csv("D:/Study/Case Studies/kaggle.com/Shelter-Animal-Outcomes-by-kaggle.com/original_data/test.csv", stringsAsFactors=FALSE)
+test = read.csv(file = "D:/Study/Case Studies/kaggle.com/Shelter-Animal-Outcomes-by-kaggle.com/original_data/test.csv",
+                stringsAsFactors=FALSE)
 test$ID = as.character(test$ID)
 Dataset = c(rep("test", length(test[,1])))
-test=data.frame(test,Dataset, stringsAsFactors = FALSE)
+test=data.frame(test,Dataset,
+                stringsAsFactors = FALSE)
 names(test)[1]=c("AnimalID")
 
 
@@ -27,7 +31,8 @@ rownames(data) = c(1:length(data[,1]))
 # splitting "AgeuponOutcome" in two different columns
 library(stringr)
 age_data=as.data.frame(t(as.data.frame(str_split(data$AgeuponOutcome, " "))))
-age_data = cbind(data$AnimalID, age_data)
+age_data = cbind(data$AnimalID,
+                 age_data)
 colnames(age_data) = c("AnimalID", "value", "unit")
 row.names(age_data) = c(1:length(age_data$value))
 # calculating age in days
@@ -43,7 +48,8 @@ for (i in 1:length(age_data$unit)){
 for (i in 1:length(temp)){
   ifelse(temp[i]==0,(temp[i]=""),(temp[i]=temp[i]))
 }
-age_data = cbind(age_data, as.numeric(temp))
+age_data = cbind(age_data,
+                 as.numeric(temp))
 colnames(age_data) = c("AnimalID", "value", "unit", "Age_in_days")
 # treating missing values in "Age_in_days" column
 temp=rep("0", times=length(age_data$Age_in_days))
@@ -55,25 +61,29 @@ for(i in 1:length(age_data$Age_in_days)){
                               ifelse(((age_data$Age_in_days[i]>730) & (age_data$Age_in_days[i]<=1825)), (temp[i]="age1825"),
                                      (temp[i]="age8030"))))))
 }
-age_data=data.frame(age_data, agecategory=temp)
+age_data=data.frame(age_data,
+                    agecategory=temp)
 # creating dummy variable for Age_in_days
 temp=unique(age_data$agecategory)
 for(i in temp){
   age_data[i]=as.numeric(ifelse((age_data$agecategory==i),1,0))
 }
-age_data=subset(age_data, select=-c(ageNA))
+age_data=subset(x = age_data,
+                select=-c(ageNA))
 age_data$AnimalID = as.character(age_data$AnimalID)
 
 
 ############################## working with AnimalType variable ###############################
 
 # creating dummy variable for AnimalType
-animal_type_data = subset(data, select=c(AnimalID, AnimalType))
+animal_type_data = subset(x = data,
+                          select=c(AnimalID, AnimalType))
 temp=unique(animal_type_data$AnimalType)
 for(i in temp){
   animal_type_data[i]=as.numeric(ifelse((animal_type_data$AnimalType==i),1,0))
 }
-animal_type_data=subset(animal_type_data, select = -c(Cat))
+animal_type_data=subset(x = animal_type_data,
+                        select = -c(Cat))
 animal_type_data$AnimalID = as.character(animal_type_data$AnimalID)
 
 
@@ -111,22 +121,30 @@ sex_data$AnimalID = as.character(sex_data$AnimalID)
 
 # splitting "Color" in two different columns
 library(stringr)
-color_data=as.data.frame(str_split(data$Color, "/"),stringsAsFactors=FALSE)
+color_data=as.data.frame(str_split(data$Color, "/"),
+                         stringsAsFactors=FALSE)
 temp=matrix(c(0),nrow(color_data),ncol(color_data))
 for(i in 1:ncol(color_data)){
   temp[,i]=as.matrix(sort(color_data[,i]))
 }
-color_data=data.frame(data$AnimalID, t(temp),stringsAsFactors=FALSE);
+color_data=data.frame(data$AnimalID,
+                      t(temp),
+                      stringsAsFactors=FALSE);
 colnames(color_data) = c("AnimalID", "color1", "color2")
 for(i in 1:length(color_data$AnimalID)){
   ifelse(color_data$color1[i]==color_data$color2[i], (color_data$color2[i]=""),(color_data$color2[i]=color_data$color2[i]))
 }
 # creating color groups for same shade of colors. eg: blue tabby, blue cream and blue will belong to color group blue.
-temp1=as.data.frame(str_split_fixed(color_data$color1, " ",2),stringsAsFactors=FALSE)
-temp1=as.data.frame(temp1[,-2],stringsAsFactors=FALSE)
-temp2=as.data.frame(str_split_fixed(color_data$color2, " ",2),stringsAsFactors=FALSE)
-temp2=as.data.frame(temp2[,-2],stringsAsFactors=FALSE)
-color_data=data.frame(color_data$AnimalID, temp1, temp2)
+temp1=as.data.frame(str_split_fixed(color_data$color1, " ",2),
+                    stringsAsFactors=FALSE)
+temp1=as.data.frame(temp1[,-2],
+                    stringsAsFactors=FALSE)
+temp2=as.data.frame(str_split_fixed(color_data$color2, " ",2),
+                    stringsAsFactors=FALSE)
+temp2=as.data.frame(temp2[,-2],
+                    stringsAsFactors=FALSE)
+color_data=data.frame(color_data$AnimalID,
+                      temp1, temp2)
 colnames(color_data) = c("AnimalID", "color1", "color2")
 # including missing values wherever applicable
 for (i in 1:length(temp)){
@@ -139,18 +157,25 @@ temp=temp[!is.na(temp)]
 for(i in temp){
   color_data[i]=as.numeric(ifelse((color_data$color1==i | color_data$color2==i),1,0))
 }
-temp = data.frame(AnimalID=color_data$AnimalID, color_data[,4:length(color_data)])
+temp = data.frame(AnimalID=color_data$AnimalID,
+                  color_data[,4:length(color_data)])
 temp[is.na(temp)]=0
 temp1=temp[c(2:length(temp))]
-temp1=subset(temp1, select = -c(White))
-color_data=data.frame(AnimalID=temp$AnimalID, color1=color_data$color1, color2=color_data$color2, temp1)
+temp1=subset(x = temp1,
+             select = -c(White))
+color_data=data.frame(AnimalID=temp$AnimalID,
+                      color1=color_data$color1,
+                      color2=color_data$color2,
+                      temp1)
 color_data$AnimalID = as.character(color_data$AnimalID)
 
 
 ################################# working with Breed variable #################################
 
 # splitting "Breed" in 3 different columns
-breed_data=data.frame(data$AnimalID, data$AnimalType, data$Breed)
+breed_data=data.frame(data$AnimalID,
+                      data$AnimalType,
+                      data$Breed)
 colnames(breed_data)=c("AnimalID", "AnimalType", "Breed")
 breed_data$Breed=as.character(breed_data$Breed)
 # working with Dog Breeds
@@ -358,8 +383,11 @@ temp1$Breed=sub("Dachshund","Hound",temp1$Breed)
 temp1$Breed=sub("Whippet","Hound",temp1$Breed)
 temp1$Breed=sub(" Mix","/Unknown",temp1$Breed)
 # final spliting of dog breeds
-temp=as.data.frame(str_split_fixed(temp1$Breed, "/",3),stringsAsFactors=FALSE)
-temp1=data.frame(temp1$AnimalID,temp1$AnimalType, temp)
+temp=as.data.frame(str_split_fixed(temp1$Breed, "/",3),
+                   stringsAsFactors=FALSE)
+temp1=data.frame(temp1$AnimalID,
+                 temp1$AnimalType,
+                 temp)
 colnames(temp1) = c("AnimalID","AnimalType", "Breed1", "Breed2", "Breed3")
 
 # working with Cat Breeds
@@ -384,8 +412,11 @@ temp2$Breed=sub("Cornish Rex","Rex",temp2$Breed)
 temp2$Breed=sub(" Mix","/Unknown",temp2$Breed)
 temp2$Breed=sub(" Shorthair","",temp2$Breed)
 # final spliting of cat breeds
-temp=as.data.frame(str_split_fixed(temp2$Breed, "/",2),stringsAsFactors=FALSE)
-temp2=data.frame(temp2$AnimalID,temp2$AnimalType, temp)
+temp=as.data.frame(str_split_fixed(temp2$Breed, "/",2),
+                   stringsAsFactors=FALSE)
+temp2=data.frame(temp2$AnimalID,
+                 temp2$AnimalType,
+                 temp)
 colnames(temp2) = c("AnimalID","AnimalType", "Breed1", "Breed2")
 
 # merging cats and dogs table as breed_data
@@ -405,22 +436,33 @@ temp=temp[!is.na(temp)]
 for(i in temp){
   breed_data[i]=as.numeric(ifelse((breed_data$Breed1==i | breed_data$Breed2==i | breed_data$Breed3==i),1,0))
 }
-temp = data.frame(AnimalID=breed_data$AnimalID, breed_data[,6:length(breed_data)])
+temp = data.frame(AnimalID=breed_data$AnimalID,
+                  breed_data[,6:length(breed_data)])
 temp[is.na(temp)]=0
 temp1=temp[c(2:length(temp))]
-temp1=subset(temp1, select = -c(Unknown))
-breed_data=data.frame(AnimalID=temp$AnimalID, AnimalType=breed_data$AnimalType, breed1=breed_data$Breed1, breed2=breed_data$Breed2, breed3=breed_data$Breed3, temp1)
+temp1=subset(x = temp1,
+             select = -c(Unknown))
+breed_data=data.frame(AnimalID=temp$AnimalID,
+                      AnimalType=breed_data$AnimalType,
+                      breed1=breed_data$Breed1,
+                      breed2=breed_data$Breed2,
+                      breed3=breed_data$Breed3,
+                      temp1)
 breed_data$AnimalID = as.character(breed_data$AnimalID)
 
 
 ########################### preparing final datasets for modelling ############################
-
-data=data.frame(subset(data, select = c(AnimalID, OutcomeType)),
+data=data.frame(subset(x = data,
+                       select = c(AnimalID, OutcomeType)),
                 Dog = animal_type_data$Dog,
-                subset(age_data, select = c(age225, age365, age730, age1825, age8030)),
-                subset(sex_data, select = -c(AnimalID, sex_subtype, sex)),
-                subset(color_data, select = -c(AnimalID, color1, color2)),
-                subset(breed_data, select = -c(AnimalID, AnimalType, breed1, breed2,breed3)),
+                subset(x = age_data,
+                       select = c(age225, age365, age730, age1825, age8030)),
+                subset(x = sex_data,
+                       select = -c(AnimalID, sex_subtype, sex)),
+                subset(x = color_data,
+                       select = -c(AnimalID, color1, color2)),
+                subset(x = breed_data,
+                       select = -c(AnimalID, AnimalType, breed1, breed2,breed3)),
                 Dataset = data$Dataset)
 # converting categorical variables into factors
 for (i in c(1:91)){
@@ -451,14 +493,23 @@ remove(i, temp, temp1, temp2, Dataset)
 # building a model on training_data
 library(randomForest)
 set.seed(1)
-forest = randomForest(x = training_data[,c(3:90)], y = training_data[,2], data = training_data, ntree = 1000, importance = TRUE)
+forest = randomForest(x = training_data[,c(3:90)],
+                      y = training_data[,2],
+                      data = training_data,
+                      ntree = 1000,
+                      importance = TRUE)
 print(forest)
 
 # predict using testing_data
-prediction = predict(object=forest, newdata=testing_data[,c(2:89)], type = "vote")
+prediction = predict(object=forest,
+                     newdata=testing_data[,c(2:89)],
+                     type = "vote")
 
 # creating the solution dataframe
-solution = data.frame(ID = testing_data$ID, prediction)
+solution = data.frame(ID = testing_data$ID,
+                      prediction)
 
 # exporting solution to solution.csv
-write.csv(solution, "D:/Study/Case Studies/kaggle.com/Shelter-Animal-Outcomes-by-kaggle.com/solution.csv", row.names=FALSE)
+write.csv(x = solution
+          file = "D:/Study/Case Studies/kaggle.com/Shelter-Animal-Outcomes-by-kaggle.com/solution.csv",
+          row.names=FALSE)
