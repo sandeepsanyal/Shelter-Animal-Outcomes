b8ea3214c3e76418f73fbccbc608eababbac9b97
Sandeep Sanyal <46559334+sandeepsanyal@users.noreply.github.com>
2019-05-08 21:07:38 +0530

    Grouping Cat and Dog Breeds

--- python_script.py
+++ python_script.py
@@ -1,16 +1,33 @@
 # importing modules
+import numpy as np
 import pandas as pd
 
-# folder paths
-raw_data_folder_path = r"C:\Users\sande\OneDrive\Github\Shelter-Animal-Outcomes-by-kaggle.com\raw_datasets" # folder containing raw_datasets
-intermediate_results_folder_path = r"C:\Users\sande\OneDrive\Github\Shelter-Animal-Outcomes-by-kaggle.com\rintermediate_results" # folder containing intermediate results
-temp_folder_path = r"C:\Users\sande\OneDrive\Github\Shelter-Animal-Outcomes-by-kaggle.com\temp" # folder containing temp_files
+# folder path
+directory = r"C:\Users\sande\OneDrive\Github\Shelter-Animal-Outcomes-by-kaggle.com"
 
 # importing datasets
-train = pd.read_csv(filepath_or_buffer=raw_data_folder_path+r"\train.csv",
+train = pd.read_csv(filepath_or_buffer=directory+r"\raw_datasets\train.csv",
                     sep=',',
                     encoding='latin-1')
-score = pd.read_csv(filepath_or_buffer=raw_data_folder_path+r"\test.csv",
+score = pd.read_csv(filepath_or_buffer=directory+r"\raw_datasets\test.csv",
                     sep=',',
                     encoding='latin-1')
 
+################################# working with Breed variable #################################
+# grouping dog breeds using AKC grouping system
+dog_breed = pd.read_excel(io=directory+r"\intermediate_results\Animal Breeds.xlsx",
+                          sheet_name="Dog Breeds").replace(to_replace=np.nan, value='')
+cat_breed = pd.read_excel(io=directory+r"\intermediate_results\Animal Breeds.xlsx",
+                          sheet_name="Cat Breeds").replace(to_replace=np.nan, value='')
+t=0
+for i in train.index:
+    t=t+1
+    if train.loc[i, 'AnimalType'] == 'Dog':
+        for j in dog_breed.index:
+            train.loc[i, 'Breed1'] = train.loc[i, 'Breed'].replace(dog_breed.loc[j, 'Breed'],
+                                                                   dog_breed.loc[j, 'BreedType'])
+    else:
+        for j in cat_breed.index:
+            train.loc[i, 'Breed1'] = train.loc[i, 'Breed'].replace(cat_breed.loc[j, 'Breed'],
+                                                                   cat_breed.loc[j, 'BreedType'])
+    print(str(round(((t/train.shape[0])*100),2))+"% done")
