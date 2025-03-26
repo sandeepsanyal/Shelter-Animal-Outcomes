d0c84b335edfdd628b0892aaeec0add6b7584ec5
Sandeep Sanyal <sandeep.sanyal@outlook.com>
2025-03-25 22:04:36 -0400

    code_update
    big_fixes

--- src/model_training.py
+++ src/model_training.py
@@ -1,8 +1,8 @@
 import sys
 
 
-home_dir = r"/Users/wrngnfreeman/Library/CloudStorage/OneDrive-Personal/shared_projects/Shelter Animal Outcomes/Shelter-Animal-Outcomes"
-data_file = r"train"
+home_dir = r"/Users/wrngnfreeman/Github/Shelter-Animal-Outcomes"
+data_file = r"Austin_Animal_Center_Outcomes_20250318"
 AnimalID=r"AnimalID"
 dep_var=r"OutcomeType"
 seed=42
